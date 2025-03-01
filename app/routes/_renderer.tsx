9bf5ce2c868d6965bfb024d276c88a3f93a1ab1a
NEKOYASAN <hello@nekoyasan.me>
2025-03-02 05:26:09 +0900

    add: meta heads

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -13,6 +13,13 @@ export default jsxRenderer(({ children }) => {
 					href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap"
 					rel="stylesheet"
 				/>
+				<meta name={'twitter:card'} content={'summary'} />
+				<meta name={'og:url'} content={'https://www.nekoyasan.me/'} />
+				<meta name={'og:title'} content={'nekoyasan'} />
+				<meta name={'og:description'} content={"nekoyasan's website"} />
+				<meta name={'description'} content={"nekoyasan's website"} />
+				<meta name={'og:image'} content={'https://www.nekoyasan.me/icon.png'} />
+				<meta name={'og:site_name'} content={'nekoyasan'} />
 				<Link href="/app/style.css" rel="stylesheet" />
 				<Script src="/app/client.ts" async />
 			</head>
